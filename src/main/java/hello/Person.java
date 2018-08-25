cc311062516d115f80d776021756ff0206384f86
signzy <nkashter@gmail.com>
2018-08-25 16:22:23 +0530

    start

---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+    @Id private String id;
+
+    private String firstName;
+    private String lastName;
+    private String occupation;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
